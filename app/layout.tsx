90de64a32d195baf70e74979efefd5bf32069909
yildizomer12 <omer1476hotmail@gmail.com>
2025-04-19 12:22:31 +0300

    Update Google site verification meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -79,7 +79,7 @@ export default async function RootLayout({
         />
         <meta
           name="google-site-verification"
-          content="AiSiarEPNKXsp4SKrEwvPx_eSv9P2V1NtrBNgM1Gosw"
+          content="v4TkfPcQIcmiJU7-RWI3oRiKhEbWbQwUS18K81XC4EY"
         />
         <script
           type="application/ld+json"
