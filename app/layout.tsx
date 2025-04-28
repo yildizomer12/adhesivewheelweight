21a3a8da194fecb8f5d1b2040692c0ab3739eafc
Git Manager User <user@example.com>
2025-04-28 18:09:35 +0300

    feat: Add Yandex Metrika tracking code (ID: 101089454)

--- app/layout.tsx
+++ app/layout.tsx
@@ -80,6 +80,24 @@ export default async function RootLayout({
         </Script>
         <noscript><div><img src="https://mc.yandex.ru/watch/95085832" style={{position:'absolute', left:'-9999px'}} alt="" /></div></noscript>
         {/* /Yandex.Metrika counter */}
+{/* Yandex.Metrika counter (101089454) */}
+        <Script id="yandex-metrika-101089454" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(101089454, "init", {
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true
+            });
+          `}
+        </Script>
+        <noscript><div><img src="https://mc.yandex.ru/watch/101089454" style={{position:'absolute', left:'-9999px'}} alt="" /></div></noscript>
+        {/* /Yandex.Metrika counter (101089454) */}
         <ClientLayout dictionary={dictionary} locale={locale}>
           {children}
         </ClientLayout>
